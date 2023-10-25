35cf6f25810df01fb085692bcb27c39089448ac6
Shahla <147069716+ShahlaAbdul@users.noreply.github.com>
2023-10-25 11:58:45 +0400

    Add files via upload

---
+++ practiseweek4/newpage.js
@@ -0,0 +1,134 @@
+
+// ? task1
+
+// let num = prompt("zehmet olmasa 1den 100e eder  eded daxil edin:");
+// let power = 1;
+// for (let i = 0; i < power; i++) {
+//     if (num % 2 == 0) {
+//         power *= num
+        
+//     }
+// }
+// console.log(power)
+
+
+// ?task2+
+// 2.Göndərilmiş 2 ədəddən 1-cisini 2-cisinin qüvvətinə yüksəldən alqoritm yazin
+
+// (example: 3 ve 5 ededleri gonderilir , 5 ustu 3 olur oda bize =>125 qayatarir.)
+// let num = parseInt(promt("ededi daxil edin"));
+// let num2 = parseInt(promt("ikinci edei daxil edin"));
+// let power = 1;
+// for (i = 0; i < num2; i++){
+//     power = num ** num;
+    
+
+// }
+// console.log(power)
+
+//                                         ?  tAASK3+
+// 1- den 1000e qeder  ededler icerisinde  reqemleri eyni olan ededlerin cemin tapin. example(11 ,222,333 ve  s)
+
+// let sum1 = 0;
+// let sum2 = 0;
+// let sum3 = 0;
+// for (let i = 0; i < 100; i++) {
+//     if (i % 11 == 0) {
+//         sum1 = sum1 + i;
+        
+//     }
+// }
+// console.log(sum1)
+// for (let i = 0; i < 1000; i++) {
+//     if (i % 111 == 0) {
+//         sum2 = sum2 + i;
+        
+//     }
+// }
+// console.log(sum2);
+// sum3 = sum1 + sum2;
+// console.log(sum3);
+// --------------------------------------------
+
+//                        ?                        task4+
+
+// Verilmis array-de cut ededlerin en boyuyunu tapan alqoritm yazin.
+// let num = 0;
+// const array1 = [1, 2, 3, 4, 6, 47, 90.45, 100];
+// for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] % 2 == 0) {
+//         if (array1[i] > num)
+//             num = array1[i];
+//     }
+    
+// }
+// console.log(num)
+
+//                                ?   task5 yarimciq
+
+// Verilmis array-de tek ededlerin en kiciyinin index-ni tapan alqoritm yazin
+
+// let num = 0;
+// let num_indeks = 0;
+// const array1 = [16, 2, 3, 4, 6, 47, 90, 45, 100,12];
+//  for (let i = 0; i < array1.length; i++) {
+//      if (array1[i] % 2 !== 0) {
+//          if (array1[i] < num)
+//              num = array1[i];
+//      }
+//      if (array1[i]=num_indeks) {
+//            num_indeks = i;
+//      }
+     
+// }
+// console.log(num_indeks)
+
+
+// ?task6
+ 
+// verilmis array - de  sade ve murekkeb ededlerin sayini tapan alqoritm yazin.
+   
+// let num4 = 0;
+// const array6 = [1, 2, 45, 13, 24, 33, 70];
+// for (let i = 0; i < array6.length; i++) {
+//     if (num4 % 2 == 0 || num4 % 3 == 0 ||)
+        
+    
+// }
+
+// console.log(num)
+
+// ?task7 .Verilmis array-de minimum ededle maksimum ededin cemini array-in ededi ortasindan boyuk olub olmadigini yoxlayin.
+
+
+
+// const array7 = [2, 45, 13, 24, 33, 70];
+// let num_min=array7[0];
+// let num_max = array7[0];
+
+
+// for (let i = 0; i < array7.length; i++) {
+//          if (num_max < array7[i]) {
+//         num_max = array7[i];
+//     }
+
+// }
+// for (let i = 0; i < array7.length; i++) {
+//     if (num_min > array7[i]) {
+//         num_min = array7[i];
+//     }
+// }
+// let sum = num_min + num_max;
+
+
+// console.log(num_min);
+// console.log(num_max);
+
+// console.log(sum)
+//   ? task8
+
+// Verilmish ededin en boyuk reqemini tapan alqoritm yazin. example: 709 en boyuk 9-dir, 37 en boyuk 7-dir.
+// let num = prompt("ededi daxil edin");
+
+
+
